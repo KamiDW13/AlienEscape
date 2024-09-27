1b286b98ce4df9983b9c568ffc9e7312152cfa74
JanWesolowski <152972200+JanWesolowski@users.noreply.github.com>
2024-09-27 20:51:29 +0200

    .

--- Assets/DoorCloseOnStart.cs
+++ Assets/DoorCloseOnStart.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorCloseOnStart : MonoBehaviour
+
 {
+    public String state;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Animator>().Play("door_1_closed");
+        GetComponent<Animator>().Play(state);
     }
 
     // Update is called once per frame
